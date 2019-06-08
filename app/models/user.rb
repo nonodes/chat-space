1fb77579a93353850fa25c90914eb039b95316a5
nonodes <laopilove2@yahoo.co.jp>
2019-06-09 02:26:17 +0900

    user_5
    メッセージ機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  #  mount_uploader :image, ImageUploader
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,5 +9,6 @@ class User < ApplicationRecord
 
   has_many :messages
   has_many :group_users
+  has_many :messages
   has_many :groups, through: :group_users
 end
