59bb53170f84e9b4cd4ccbc55dcbcfc7304b5d2c
nonodes <laopilove2@yahoo.co.jp>
2019-06-19 14:04:04 +0900

    追加ボタンと削除ボタンを追加しました。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,20 +1,31 @@
 
-$(function(){
+$(function () {
 
   var search_list = $(".chat-group-form__input");
 
-function appendProduct(user) {
+  function appendProduct(user) {
 
-  var html = `<div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user clearfix">
               <p class="chat-group-user__name">${user.name}</p>
-              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="ユーザー名">追加</div>
-              </div>`    
+              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>`
 
-$('#user-search-result').append(html);
+    $('#user-search-result').append(html);
 
-}
+  }
 
-$(".chat-group-form__input").on("keyup", function() {
+  function appendpost(name) {
+
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+      return html;
+
+  }
+
+  $(".chat-group-form__input").on("keyup", function () {
     var input = $(this).val();
 
 
@@ -26,20 +37,38 @@ $(".chat-group-form__input").on("keyup", function() {
     })
 
 
-    .done(function(users){
+    .done(function (users) {
       $(".chat-group-form__input").empty();
-        if (users.length !== 0) {
-          users.forEach(function(user){
-            appendProduct(user);
+      if (users.length !== 0) {
+        users.forEach(function (user) {
+          appendProduct(user);
         });
-        }
-        else {
-          appendErrMsgToHTML("一致する名前はありません");
-        }
+      }
+      else {
+        appendErrMsgToHTML("一致する名前はありません");
+      }
 
     })
-    .fail(function(){
-        alert('エラー');
-    })	
-})
+    .fail(function () {
+      alert('エラー');
+    })
+  })
+  
+  $(document).on("click",".user-search-add" ,function () {
+    //追加ボタンをクリックされたら、チャットメンバーのviewに追加したいuserが追加される。
+    // 追加ボタンを押された所の名前が必要。
+    var name = $(this).data('user-name')
+    console.log(this)
+    var html = appendpost(name)
+    $('#chat-group-users').append(html);
+    //検索したてんらさんを削除したい。 
+    $('#user-search-result').empty();
+  })
+  $(document).on("click",".user-search-remove" ,function () {
+    // 削除ボタンを押されたら、グループに追加されたuserを削除する。
+    // 名前は一致する条件のuserを削除する。
+    $(this).parent().remove();
+    console.log(this);
+    
+  })
 })
